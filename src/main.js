30d8711fa9c7b04e62ccb553b2289fda1f2929b2
AlexRashidov <rashidov.sasha@yandex.ru>
2026-02-06 02:08:46 +0700

    delete test alert main js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
-alert('main.js стартовал')
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-alert('Vue смонтирован')
