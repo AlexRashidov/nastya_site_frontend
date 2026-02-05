ba204a145f664a6858b7f33176a265bb68b8e5c6
AlexRashidov <rashidov.sasha@yandex.ru>
2026-02-06 01:24:37 +0700

    add test alert in main js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
+alert('main.js стартовал')
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+alert('Vue смонтирован')
